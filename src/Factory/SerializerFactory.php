031b11dd77343d54c0e5f521a802beffe88249cf
Christoph Stickel <christoph@neochic.de>
2015-04-10 16:33:42 +0200

    * added jms serializer annotations to doctrine annotation registry

--- src/Factory/SerializerFactory.php
+++ src/Factory/SerializerFactory.php
@@ -2,11 +2,13 @@
 namespace Neochic\SlimTools\Factory;
 
 use JMS\Serializer\SerializerBuilder;
+use \Doctrine\Common\Annotations\AnnotationRegistry;
 
 class SerializerFactory
 {
-    public static function createSerializer(SerializerBuilder $serializerBuilder)
+    public static function createSerializer(array $options, SerializerBuilder $serializerBuilder)
     {
+        AnnotationRegistry::registerAutoloadNamespace('JMS\Serializer\Annotation', $options['annotation_path']);
         return $serializerBuilder->build();
     }
 }
