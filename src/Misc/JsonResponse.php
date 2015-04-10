09293b8e21842f0550f0b4c6caa43425930d5ca7
Christoph Stickel <christoph@neochic.de>
2015-04-10 15:11:15 +0200

    * added default value for context parameter in JsonResponse

--- src/Misc/JsonResponse.php
+++ src/Misc/JsonResponse.php
@@ -4,22 +4,22 @@ namespace Neochic\SlimTools\Misc;
 
 class JsonResponse extends JSend
 {
-    public function success($data = null, $context)
+    public function success($data = null, $context = null)
     {
         return addContext(parent::success($data), $context);
     }
 
-    public function fail($type, $data = null, $context)
+    public function fail($type, $data = null, $context = null)
     {
         return addContext(parent::fail($type, $data), $context);
     }
 
-    public function error($message, $context)
+    public function error($message, $context = null)
     {
         return addContext(parent::error($message), $context);
     }
 
-    protected function addContext($response, $context) {
+    protected function addContext($response, $context = null) {
         return array(
             'json' => $response,
             'context' => $context
