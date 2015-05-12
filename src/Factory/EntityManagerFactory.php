523fed1373bcdf1361b0a434015958b51f8eea82
Franz-R. Reinhart <franz@neochic.de>
2015-05-12 18:20:37 +0200

    * refactored autoloading

--- src/Factory/EntityManagerFactory.php
+++ src/Factory/EntityManagerFactory.php
@@ -3,6 +3,7 @@ namespace Neochic\SlimTools\Factory;
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 class EntityManagerFactory
 {
@@ -11,6 +12,10 @@ class EntityManagerFactory
 
         $config = Setup::createAnnotationMetadataConfiguration($doctrine['model.paths'], $isDevMode, null, null, false);
         $config->setAutoGenerateProxyClasses(constant("Doctrine\\Common\\Proxy\\AbstractProxyFactory::". $doctrine['proxyMode']));
+
+        foreach($doctrine['annotation.paths'] as $namespace => $path) {
+            AnnotationRegistry::registerAutoloadNamespace($namespace, $path);
+        }
 
         return EntityManager::create($database, $config);
     }
--- src/Factory/SerializerFactory.php
+++ src/Factory/SerializerFactory.php
@@ -2,14 +2,12 @@
 namespace Neochic\SlimTools\Factory;
 
 use JMS\Serializer\SerializerBuilder;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use JMS\Serializer\Naming\PropertyNamingStrategyInterface;
 
 class SerializerFactory
 {
-    public static function createSerializer(array $options, SerializerBuilder $serializerBuilder, PropertyNamingStrategyInterface $namingStrategy)
+    public static function createSerializer(SerializerBuilder $serializerBuilder, PropertyNamingStrategyInterface $namingStrategy)
     {
-        AnnotationRegistry::registerAutoloadNamespace('JMS\Serializer\Annotation', $options['annotation_path']);
         $serializerBuilder->setPropertyNamingStrategy($namingStrategy);
         return $serializerBuilder->build();
     }
