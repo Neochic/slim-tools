c9d31decb5df91a44fc82d2beb1d19c1bb39a26c
Franz-R. Reinhart <franz@neochic.de>
2015-07-29 11:09:45 +0200

    * changed initialization (mainly due to doctrine validator's annotations using psr-4)

--- src/Core.php
+++ src/Core.php
@@ -4,12 +4,17 @@ namespace Neochic\SlimTools;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Composer\Autoload\ClassLoader;
 
 class Core
 {
-    public function __construct($config, $boot = 'app')
+    public function __construct($config, $boot = 'app', ClassLoader $classLoader = null)
     {
         $container = new ContainerBuilder();
+        if(!$classLoader) {
+            $classLoader = new ClassLoader();
+        }
+        $container->set('classLoader', $classLoader);
         $loader = new YamlFileLoader($container, new FileLocator());
         $loader->load(__DIR__ . '/../config/core.yml');
         $loader->load($config);
--- src/Factory/EntityManagerFactory.php
+++ src/Factory/EntityManagerFactory.php
@@ -4,15 +4,17 @@ namespace Neochic\SlimTools\Factory;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 
 class EntityManagerFactory
 {
-    public static function createEntityManager(array $database, array $doctrine) {
+    public static function createEntityManager(array $database, array $doctrine, ClassLoader $classLoader) {
         $isDevMode = true;
 
         $config = Setup::createAnnotationMetadataConfiguration($doctrine['model.paths'], $isDevMode, null, null, false);
         $config->setAutoGenerateProxyClasses(constant("Doctrine\\Common\\Proxy\\AbstractProxyFactory::". $doctrine['proxyMode']));
 
+        AnnotationRegistry::registerLoader(array($classLoader, 'loadClass'));
         foreach($doctrine['annotation.paths'] as $namespace => $path) {
             AnnotationRegistry::registerAutoloadNamespace($namespace, $path);
         }
