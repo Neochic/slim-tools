e7d5677f9c63319dabfbb158a218c832773f086f
Christoph Stickel <christoph@neochic.de>
2018-02-15 20:37:54 +0100

    * dql functions can be added to em via config

--- src/Factory/EntityManagerFactory.php
+++ src/Factory/EntityManagerFactory.php
@@ -10,13 +10,21 @@ class EntityManagerFactory
 {
     public static function createEntityManager(array $database, array $doctrine, ClassLoader $classLoader) {
         $isDevMode = true;
-	    $config = Setup::createAnnotationMetadataConfiguration($doctrine['model.paths'], $isDevMode, null, null, false);
+	$config = Setup::createAnnotationMetadataConfiguration($doctrine['model.paths'], $isDevMode, null, null, false);
         $config->setAutoGenerateProxyClasses(constant("Doctrine\\Common\\Proxy\\AbstractProxyFactory::". $doctrine['proxyMode']));
 
         AnnotationRegistry::registerLoader(array($classLoader, 'loadClass'));
 
-        foreach($doctrine['annotation.paths'] as $namespace => $path) {
-            AnnotationRegistry::registerAutoloadNamespace($namespace, $path);
+        if (isset($doctrine['annotation.paths'])) {
+            foreach ( $doctrine['annotation.paths'] as $namespace => $path ) {
+	        AnnotationRegistry::registerAutoloadNamespace( $namespace, $path );
+            }
+        }
+
+        if (isset($doctrine['dql.paths'])) {
+            foreach($doctrine['dql.paths'] as $name => $path) {
+                $config->addCustomStringFunction($name, $path);
+            }
         }
 
         $em = EntityManager::create($database, $config);
